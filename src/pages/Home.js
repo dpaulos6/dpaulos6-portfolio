5df55f81af62e4f92fbbbf72e16993c3b3dfc486
Diogo Paulos <51181228+dpaulos6@users.noreply.github.com>
2023-01-27 19:19:08 +0000

    Updated Home Page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,36 @@
 import React from "react"
+import { TypeAnimation } from "react-type-animation"
 
 const Home = () => {
   return (
     <>
-      <div className="h-screen">
-
+      <div className="h-[600px] max-h-[600px] overflow-hidden relative"> {/* Short introduction */}
+        <div className="flex h-full">
+          <div className="mx-auto my-auto text-center w-max">
+            <p className="text-2xl md:text-4xl lg:text-6xl home-title">
+              <code>Hi, I'm Diogo Paulos!</code>
+            </p>
+            <p className="text-lg sm:text-xl md:text-2xl lg:text-4xl home-title">
+              <TypeAnimation
+                sequence={[
+                  'UI/UX Designer',
+                  5000,
+                  'Web/Software Developer',
+                  5000,
+                ]}
+                wrapper="code"
+                cursor={true}
+                repeat={Infinity}
+                speed="60"
+              />
+            </p>
+          </div>
+        </div>
+        <div className="circle-1"><span className="sr-only">Circle 1 - 512x512</span></div>
+        <div className="circle-2"><span className="sr-only">Circle 2 - 512x512</span></div>
+        <div className="circle-3"><span className="sr-only">Circle 3 - 175x175</span></div>
+        <div className="circle-4"><span className="sr-only">Circle 4 - 125x125</span></div>
+        <div className="circle-5"><span className="sr-only">Circle 5 - 60x60</span></div>
       </div>
     </>
   );
