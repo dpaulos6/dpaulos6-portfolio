ecc7b103835a30a3d13faa88edd4651c523146e6
Diogo Paulos <51181228+dpaulos6@users.noreply.github.com>
2024-03-03 14:03:58 +0000

    Deployment Fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import './global.css'
+import { Analytics } from "@vercel/analytics/react"
 
 // Pages
 import Home from './pages/Home.tsx'
@@ -13,6 +14,7 @@ import Footer from './components/Footer'
 export default function App() {
   return (
     <>
+      <Analytics/>
       <Navbar/>
       <Router>
         <Routes>
